9f995e64740351d1b1b9001f965c15a37a2a67a4
Conrad Sollitt <csollitt@hotmail.com>
2019-12-09 15:33:22 -0800

    :robot: Server Sync Script Update

--- scripts/sync-server-from-github.sh
+++ scripts/sync-server-from-github.sh
@@ -17,13 +17,14 @@
 # -----------------------------------------------------------------------------
 
 wget https://github.com/fastsitephp/playground/archive/master.zip -O ~/master.zip
-unzip -q master.zip
-rm master.zip
+unzip -q ~/master.zip
+rm ~/master.zip
 wget https://github.com/fastsitephp/fastsitephp/archive/master.zip -O ~/master.zip
-unzip -q master.zip
-rm master.zip
+unzip -q ~/master.zip
+rm ~/master.zip
 rsync -rcv --delete ~/playground-master/app/ /var/www/app
 rsync -rcv --delete ~/playground-master/app_data/template/ /var/www/app_data/template
+rsync -rcv --delete ~/playground-master/scripts/ /var/www/scripts
 rsync -rcv --delete --exclude sites ~/playground-master/html/ /var/www/html
 rsync -rcv --delete ~/fastsitephp-master/src/ /var/www/vendor/fastsitephp/src
 rm -r ~/playground-master
